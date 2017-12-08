e1ed2618ff750c5f001705be9ec6a37a6c808d51
hmaruyama <maruyama@val.co.jp>
2017-12-08 17:11:44 +0900

    正常に動いてないけどコミット

--- ekispert.js
+++ ekispert.js
@@ -1,13 +1,22 @@
 module.exports = function(RED) {
 
-    function StationNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
-    }
+  function StationNode(config) {
+    RED.nodes.createNode(this, config);
 
-    RED.nodes.registerType("aaaaa",StationNode);
+    this.accessKey = config.accessKey;
+    console.log("accesskey:" + this.accessKey);
+    this.stationName = config.stationName;
+    this.stationCode = config.stationCode;
+    var node = this;
+
+    node.on('input', function(msg) {
+
+      var accessKey = node.accessKey;
+      console.log("accesskey: " + accessKey);
+      node.send(msg);
+
+    });
+  }
+
+  RED.nodes.registerType("aaaaa",StationNode);
 }
