daff20b2db2dc050e640d7f8464070d86bee7144
hmaruyama <maruyama@val.co.jp>
2017-12-08 20:39:39 +0900

    駅を名前指定で検索できるようにしました。

--- ekispert.js
+++ ekispert.js
@@ -1,15 +1,19 @@
 module.exports = function(RED) {
 
   var request = require('request');
+  var endpoint = "https://api.ekispert.jp/v1/json/";
 
   function LowerCaseNode(config) {
     RED.nodes.createNode(this,config);
 
     this.accessKey = config.accessKey;
+    this.stationName = config.stationName;
+    this.stasionCode = config.stasionCode;
     var node = this;
+
     node.on('input', function(msg) {
-      var url = encodeURI("https://api.ekispert.jp/v1/json/station?key=" + node.accessKey);
-      // node.send(msg);
+
+      var url = encodeURI(endpoint + "station?key=" + node.accessKey + "&name=" + node.stationName);
 
       request(url, function (error, response, body) {
         if (!error) {
