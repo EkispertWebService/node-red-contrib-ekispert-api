0b49d587510b488ceb92bb658b0cbc97d11442e7
hmaruyama <maruyama@val.co.jp>
2017-12-08 20:01:01 +0900

    インデントを合わせました。

--- ekispert.js
+++ ekispert.js
@@ -1,14 +1,14 @@
 module.exports = function(RED) {
-    function LowerCaseNode(config) {
-        RED.nodes.createNode(this,config);
+  function LowerCaseNode(config) {
+    RED.nodes.createNode(this,config);
 
-        console.log(config.accessKey);
+    console.log(config.accessKey);
 
-        var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("ekispert",LowerCaseNode);
+    var node = this;
+    node.on('input', function(msg) {
+      msg.payload = msg.payload.toLowerCase();
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("ekispert",LowerCaseNode);
 }
