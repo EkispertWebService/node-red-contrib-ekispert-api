1a34009155330690587d4ec8e1ef2a6c2deed946
hmaruyama <maruyama@val.co.jp>
2017-12-08 15:48:26 +0900

    アクセスキーの入力欄を追加しました。

--- ekispert.js
+++ ekispert.js
@@ -1,5 +1,6 @@
 module.exports = function(RED) {
-    function LowerCaseNode(config) {
+
+    function StationNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
@@ -7,5 +8,6 @@ module.exports = function(RED) {
             node.send(msg);
         });
     }
-    RED.nodes.registerType("aaaaa",LowerCaseNode);
+
+    RED.nodes.registerType("aaaaa",StationNode);
 }
