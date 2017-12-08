aaced7dd050c8d86ba4e573d641c1d6f35a34e79
hmaruyama <maruyama@val.co.jp>
2017-12-09 03:28:33 +0900

    /stationの全てのパラメータに対応しました。

--- ekispert.js
+++ ekispert.js
@@ -9,6 +9,17 @@ module.exports = function(RED) {
     this.accessKey = config.accessKey;
     this.stationName = config.stationName;
     this.stationCode = config.stationCode;
+    this.stationOldName = config.stationOldName;
+    this.corporationName = config.corporationName;
+    this.railName = config.railName;
+    this.operationLineCode = config.operationLineCode;
+    this.trafficType = config.trafficType;
+    this.prefectureCode = config.prefectureCode;
+    this.offset = config.offset;
+    this.limit = config.limit;
+    this.direction = config.direction;
+    this.corporationBind = config.corporationBind;
+    this.gcs = config.gcs;
     var node = this;
 
     node.on('input', function(msg) {
@@ -16,7 +27,18 @@ module.exports = function(RED) {
       var params = {
         key: node.accessKey,
         name: node.stationName,
-        code: node.stationCode
+        code: node.stationCode,
+        oldName: node.stationOldName,
+        corporationName: node.corporationName,
+        railName: node.railName,
+        operationLineCode: node.operationLineCode,
+        type: node.trafficType,
+        prefectureCode: node.prefectureCode,
+        offset: node.offset,
+        limit: node.limit,
+        direction: node.direction,
+        corporationBind: node.corporationBind,
+        gcs: node.gcs
       }
 
       var flatParams = "";
