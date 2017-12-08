827958e7a4eba1dfc4b3e9c18e2262e4f54754d4
hmaruyama <maruyama@val.co.jp>
2017-12-08 20:25:33 +0900

    駅すぱあとwebサービスにリクエストを送ってデバッグコンソールに出力するところまで実装しました。

--- ekispert.js
+++ ekispert.js
@@ -1,14 +1,24 @@
 module.exports = function(RED) {
 
+  var request = require('request');
+
   function LowerCaseNode(config) {
     RED.nodes.createNode(this,config);
 
-    console.log("key1:" + config.accessKey);
     this.accessKey = config.accessKey;
     var node = this;
     node.on('input', function(msg) {
-      console.log("key: " + node.accessKey);
-      node.send(msg);
+      var url = encodeURI("https://api.ekispert.jp/v1/json/station?key=" + node.accessKey);
+      // node.send(msg);
+
+      request(url, function (error, response, body) {
+        if (!error) {
+          msg.payload = JSON.parse(body);
+          node.send(msg);
+        } else {
+          node.error(error);
+        }
+      });
     });
   }
   RED.nodes.registerType("ekispert",LowerCaseNode);
